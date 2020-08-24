754d87c796457a6b8e8e7d63fc14cf3234a9454b
ddryo <31400297+ddryo@users.noreply.github.com>
2020-08-24 21:10:19 +0900

    delete build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a7677ed2041e5bff20d5482f17b939ad');
